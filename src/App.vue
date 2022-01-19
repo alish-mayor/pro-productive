54fc3305470b5fbda35b58c68f0186d159c3d5a0
alish-mayor <alish22061999@gmail.com>
2022-01-19 22:24:02 +0600

    added basic logic of time tracker

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,58 @@
 <template>
   <div id="app">
-    <h1>Hello</h1>
-    <p>Something</p>
+    <h1><span class="hours">{{ parseTime(hours) }}</span>:<span class="minutes">{{parseTime(minutes)}}</span>:<span class="seconds">{{parseTime(seconds)}}</span></h1>
+    <div class="buttons">
+      <button class="btn btn_start" @click="start"><i class='bx bx-play'></i></button>
+      <button class="btn btn_stop" @click="stop"><i class='bx bx-stop' ></i></button>
+    </div>
+    
   </div>
 </template>
+
+<script>
+
+export default({
+  data(){
+    return{
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      interval: 0,
+    }
+  },
+
+  methods: {
+    start(){
+      this.interval = setInterval(() => {
+        this.seconds++;
+
+        if(this.seconds === 60){
+          this.seconds = 0;
+          this.minutes++;
+        }
+
+        if(this.minutes === 60){
+          this.minutes = 0;
+          this.hours++;
+        }
+      }, 1000);
+      const btn = document.querySelector('.btn_start');
+      btn.disabled = true;
+    },
+    stop(){
+      clearInterval(this.interval);
+      this.hours = this.minutes = this.seconds = 0;
+      const btn = document.querySelector('.btn_start');
+      btn.disabled = false;
+    },
+    parseTime(time){
+      return time.toString().padStart(2, 0);
+    }
+  }
+  
+})
+</script>
+
 
 <style lang="scss">
 *, 
@@ -19,5 +68,24 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #191b1d;
+  text-align: center;
+}
+
+.buttons{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.btn{
+  padding: 0.2rem;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  
+  &:not(:first-child){
+    margin-left: 0.5rem;
+  }
 }
 </style>
