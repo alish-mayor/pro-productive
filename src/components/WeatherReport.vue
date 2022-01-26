67c6ada3e88afe2fb8aabc318484974e027a73d8
alish-mayor <alish22061999@gmail.com>
2022-01-26 23:53:03 +0600

    added basic logic to weather report

--- src/components/WeatherReport.vue
+++ src/components/WeatherReport.vue
@@ -1,11 +1,39 @@
 <template>
     <div class="weather">
         <h1>Weather report</h1>
+        <p>Temp: {{ temp }}ºC</p>
+        <p>Wind speed: {{ speed }}m/s</p>
+        <p>Humidity: {{ humidity }}%</p>
     </div>
 </template>
 
 <script>
+import API_KEY from '../api_key.js';
+
 export default {
+    data(){
+        return{
+            temp: '',
+            speed: '',
+            humidity: '',
+        }
+    },
+    mounted(){
+        this.loadData();
+    },
+    
+    methods: {
+        async loadData(){
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`);
+            const data = await res.json();
+            this.setData(data);
+        },
+        setData(data){
+            this.temp = data.main.temp;
+            this.speed = data.wind.speed;
+            this.humidity = data.main.humidity;
+        },
+    }
     
 }
 </script>
